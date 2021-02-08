be9bbf86670e322fa45b89cb5c0113767fa105b9
Lucas Martim <l_martim@yahoo.com.br>
2021-02-08 00:17:41 -0300

    updating app js

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <ButtonPrimary>
-    Button label
-  </ButtonPrimary>
-  <HeadingSmall>
-    Heading small
-  </HeadingSmall>
-  <SubtitleSmall>
-    Subtitle small
-  </SubtitleSmall>
-  <Shape>
-    <Paragraph>
-      Lorem ipsum etc
-    </Paragraph>
-  </Shape>
-  <br>
   <CardContent
     title="Card Content Title"
     subtitle="Card Content Subtitle"
@@ -25,33 +9,12 @@
 </template>
 
 <script>
-import ButtonPrimary from './components/ButtonPrimary'
-import HeadingSmall from './components/HeadingSmall'
-import SubtitleSmall from './components/SubtitleSmall'
-import Paragraph from './components/Paragraph'
-import Shape from './components/Shape'
 import CardContent from './components/CardContent'
 
 export default {
   name: 'App',
   components: {
-    ButtonPrimary,
-    HeadingSmall,
-    SubtitleSmall,
-    Paragraph,
-    Shape,
     CardContent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
